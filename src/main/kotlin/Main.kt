60a0c5ef4f203b43737361ab046e0ff570a111f2
Claudio de Souza <cscriptori@yahoo.com>
2021-12-11 11:42:59 -0500

    Adding CountingSortTest and remove it from Main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,22 +1,4 @@
-import me.scriptori.FibonacciSequence
-import me.scriptori.aVeryBigSum
-import me.scriptori.birthdayCakesCandles
-import me.scriptori.caesarCipher
-import me.scriptori.compareTriplets
-import me.scriptori.countingSort
-import me.scriptori.diagonalDifference
-import me.scriptori.findMedian
-import me.scriptori.fizzBuzz
-import me.scriptori.gradingStudents
-import me.scriptori.lonelyInteger
-import me.scriptori.makeMove
-import me.scriptori.miniMaxSum
-import me.scriptori.plusMinus
-import me.scriptori.resetTikTokToe
-import me.scriptori.simpleArraySum
-import me.scriptori.sortResults
-import me.scriptori.staircase
-import me.scriptori.timeConversion
+import me.scriptori.*
 
 fun main() {
     println("========= FibonacciSequence ========")
@@ -48,29 +30,19 @@ fun main() {
             )
         )
     )
-    println("=========== countingSort ===========")
-    val ret1 = countingSort(
-        arrayOf(
-            63, 25, 73, 1, 98, 73, 56, 84, 86, 57, 16, 83, 8, 25, 81, 56, 9, 53, 98, 67, 99, 12,
-            83, 89, 80, 91, 39, 86, 76, 85, 74, 39, 25, 90, 59, 10, 94, 32, 44, 3, 89, 30, 27,
-            79, 46, 96, 27, 32, 18, 21, 92, 69, 81, 40, 40, 34, 68, 78, 24, 87, 42, 69, 23, 41,
-            78, 22, 6, 90, 99, 89, 50, 30, 20, 1, 43, 3, 70, 95, 33, 46, 44, 9, 69, 48, 33, 60,
-            65, 16, 82, 67, 61, 32, 21, 79, 75, 75, 13, 87, 70, 33
-        )
-    )
-    println("Counting Sort = " + ret1?.joinToString(" "))
-    println("Sort Results = " + sortResults(ret1).joinToString(" "))
-    val ret2 = countingSort(arrayOf(6, 7, 7, 3, 5, 1, 9, 2, 6, 2))
-    println("Counting Sort = " + ret2?.joinToString(" "))
-    println("Sort Results = " + sortResults(ret2).joinToString(" "))
+
     println("=========== caesarCipher ===========")
     println(caesarCipher("There's a starman waiting in the sky", 3))
+
     println("============= staircase ============")
     staircase(6)
+
     println("========== simpleArraySum ==========")
     println(simpleArraySum(arrayOf(1, 2, 3, 4, 10, 11)))
+
     println("============ aVeryBigSum ===========")
     println(aVeryBigSum(arrayOf(1000000001, 1000000002, 1000000003, 1000000004, 1000000005)))
+
     println("============ compareTriplets ===========")
     println(compareTriplets(arrayOf(5, 6, 7), arrayOf(3, 6, 10)).joinToString(" "))
     println("========= birthdayCakesCandles =========")
--- src/main/kotlin/me/scriptori/CountingSort.kt
+++ src/main/kotlin/me/scriptori/CountingSort.kt
@@ -1,21 +1,49 @@
 package me.scriptori
 
-fun countingSort(arr: Array<Int>): Array<Int>? {
-    return arr.maxOrNull()?.let { max ->
+/**
+ * Problem to solve: Sort an integer array using counting sort:
+ *    - Input: [6, 7, 7, 3, 5, 1, 9, 2, 6, 2]
+ *    - Output: [1, 2, 2, 3, 5, 6, 6, 7, 7, 9]
+ *
+ *    Tip:
+ *    Counting sorting for the input above would be [0, 1, 2, 1, 0, 1, 2, 2, 0, 1] where:
+ *    0 -> 0 count
+ *    1 -> 1 count
+ *    2 -> 2 counts
+ *    3 -> 1 count
+ *    4 -> 0 count
+ *    5 -> 1 count
+ *    6 -> 2 counts
+ *    7 -> 2 counts
+ *    8 -> 0 counts
+ *    9 -> 1 count
+ */
+
+fun countingSort(arr: Array<Int>): Array<Int> {
+    return arr.maxOrNull()?.let { max ->   // Get the max integer in the array -> 9
+        // initialize the returned array with zeros for the size based on the max number
         val result = Array(max + 1) { 0 }
+        // iterates through all numbers in the original array
         for (i in arr.indices) {
+            // increase the counter for number in the array position
             result[arr[i]] = result[arr[i]] + 1
         }
+        // return the results
         result
-    }
+    } ?: emptyArray()
 }
 
 fun sortResults(countArr: Array<Int>?): Array<Int> {
+    // initialize the returned array
     val result = mutableListOf<Int>()
+    // iterates through the given array if it is not null
     countArr?.forEachIndexed { index, i ->
+        // repeat n times the value
         repeat(i) {
+            // add the value to the returned array
             result.add(index)
         }
     }
+    // return the results
     return result.toTypedArray()
 }
